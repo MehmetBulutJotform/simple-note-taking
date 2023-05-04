3985f480cf5f59d5c68382dc5446668cbc75d906
MehmetBulutJotform <mehmetbulut@jotform.com>
2023-05-04 19:43:25 +0300

    First Commit of the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+
+const notesSlice = createSlice({
+  name: 'notes',
+  initialState: [],
+  reducers: {
+    addNote: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    notes: notesSlice.reducer,
+  },
+});
 
 function App() {
+  const [newNote, setNewNote] = useState('');
+  const notes = useSelector((state) => state.notes);
+  const dispatch = useDispatch();
+
+  const handleInputChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      dispatch(notesSlice.actions.addNote(newNote));
+      setNewNote('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Simple Notetaking App with Redux</h1>
+      <input type="text" value={newNote} onChange={handleInputChange} onKeyDown={handleKeyDown} />
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function AppWithRedux() {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+}
+
+export default AppWithRedux;
